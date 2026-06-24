3af8fabfc29e3dce7cd14e1870d8c422644a58bb
TaiJiYu <taijiyugame@163.com>
2026-06-24 14:06:02 +0800

    命令前缀

--- pkg/netfinder/code.go
+++ pkg/netfinder/code.go
@@ -21,6 +21,8 @@ const (
 const (
 	afterMask    byte = 0x1f
 	netOrderMask byte = ^afterMask
+	// 协议魔数字符串，用于标识本软件的消息，避免误识别其他软件的广播
+	protocolMagic = "GMStudio:GMSenderOrder:Broadcast"
 )
 
 // 建联基础信息
@@ -37,22 +39,27 @@ func (b baseInfo) addr() string {
 // 解码与编码
 func askMasterBytes() []byte {
 	header := byte(askMasterNetOrder) | (byte(readWaitSec()) & afterMask)
-	return append([]byte{header}, readSelfBaseInfoBytes()...)
+	return append([]byte(protocolMagic), append([]byte{header}, readSelfBaseInfoBytes()...)...)
 }
 
 // master回应
 func masterAnswerBytes() []byte {
 	header := byte(masterAnswerNetOrder)
-	return append([]byte{header}, readSelfBaseInfoBytes()...)
+	return append([]byte(protocolMagic), append([]byte{header}, readSelfBaseInfoBytes()...)...)
 }
 
 func (f *finder) decode(data []byte) bool {
-	if len(data) < 1 {
+	magicLen := len(protocolMagic)
+	if len(data) < magicLen+1 {
 		return false
 	}
-	order := decodeOrder(data[0])
+	// 首先检查协议魔数
+	if string(data[:magicLen]) != protocolMagic {
+		return false
+	}
+	order := decodeOrder(data[magicLen])
 	info := baseInfo{}
-	err := json.Unmarshal(data[1:], &info)
+	err := json.Unmarshal(data[magicLen+1:], &info)
 	if err != nil {
 		return false
 	}
@@ -65,7 +72,7 @@ func (f *finder) decode(data []byte) bool {
 		//  询问指令
 		if !CompetitiveMaster(info) {
 			// 竞争失败主动退让
-			waitSec := (time.Duration(data[0]&afterMask) + 1) * time.Second
+			waitSec := (time.Duration(data[magicLen]&afterMask) + 1) * time.Second
 			f.askWait(waitSec)
 		}
 		return false
@@ -94,34 +101,39 @@ func CompetitiveMaster(otherInfo baseInfo) bool {
 // 询问现在的公开文件列表
 func askFilesBytes() []byte {
 	header := byte(askFilesNetOrder)
-	return []byte{header}
+	return append([]byte(protocolMagic), header)
 
 }
 func publicSelfFileBytes(f File) []byte {
 	header := byte(publicFileNetOrder)
 	data, _ := json.Marshal(f)
-	return append([]byte{header}, data...)
+	return append([]byte(protocolMagic), append([]byte{header}, data...)...)
 }
 
 // 删除自己的公开文件
 func delPublicSelfFileBytes(f File) []byte {
 	header := byte(delfPublicFileNetOrder)
 	data, _ := json.Marshal(f)
-	return append([]byte{header}, data...)
+	return append([]byte(protocolMagic), append([]byte{header}, data...)...)
 }
 
 // 某个节点解码组播消息
 // 返回来源的基础信息和是否需要回复
 func (f *finder) nodeDecode(data []byte) {
-	if len(data) < 1 {
+	magicLen := len(protocolMagic)
+	if len(data) < magicLen+1 {
 		return
 	}
-	order := decodeOrder(data[0])
+	// 首先检查协议魔数
+	if string(data[:magicLen]) != protocolMagic {
+		return
+	}
+	order := decodeOrder(data[magicLen])
 	switch order {
 	case masterAnswerFilesNetOrder:
 		//  master回复的文件列表
 		files := []File{}
-		err := json.Unmarshal(data[1:], &files)
+		err := json.Unmarshal(data[magicLen+1:], &files)
 		if err != nil {
 			return
 		}
@@ -137,7 +149,7 @@ func (f *finder) masterAnswerFiles() {
 		// 文件列表序列化失败，一般不太可能
 		return
 	} else {
-		filesOrdBytes := append([]byte{byte(masterAnswerFilesNetOrder)}, dataFiles...)
+		filesOrdBytes := append([]byte(protocolMagic), append([]byte{byte(masterAnswerFilesNetOrder)}, dataFiles...)...)
 		for i := 0; i < 3; i++ {
 			if _, err := f.broadcastCoon.WriteToUDP(filesOrdBytes, broadcastAddr); err != nil {
 				time.Sleep(time.Second)
@@ -151,15 +163,20 @@ func (f *finder) masterAnswerFiles() {
 // master解码组播消息
 // 返回是否放弃了master身份
 func (f *finder) masterDecode(data []byte) bool {
-	if len(data) < 1 {
+	magicLen := len(protocolMagic)
+	if len(data) < magicLen+1 {
 		return false
 	}
-	order := decodeOrder(data[0])
+	// 首先检查协议魔数
+	if string(data[:magicLen]) != protocolMagic {
+		return false
+	}
+	order := decodeOrder(data[magicLen])
 	switch order {
 	case masterAnswerNetOrder:
 		// 收到了master回复
 		info := baseInfo{}
-		err := json.Unmarshal(data[1:], &info)
+		err := json.Unmarshal(data[magicLen+1:], &info)
 		if err != nil {
 			return false
 		}
@@ -182,7 +199,7 @@ func (f *finder) masterDecode(data []byte) bool {
 	case publicFileNetOrder:
 		// 收到了节点的公开请求
 		file := File{}
-		if err := json.Unmarshal(data[1:], &file); err != nil {
+		if err := json.Unmarshal(data[magicLen+1:], &file); err != nil {
 			// 文件有问题
 			return false
 		}
@@ -195,7 +212,7 @@ func (f *finder) masterDecode(data []byte) bool {
 	case delfPublicFileNetOrder:
 		// 收到了删除公开文件的请求
 		file := File{}
-		if err := json.Unmarshal(data[1:], &file); err != nil {
+		if err := json.Unmarshal(data[magicLen+1:], &file); err != nil {
 			// 文件有问题
 			return false
 		}
@@ -225,20 +242,26 @@ func downLoadFileBytes(fileName, pubKey string) []byte {
 		PubKey:   pubKey,
 	}
 	data, _ := json.Marshal(req)
-	return append([]byte{byte(downLoadFileNetOrder)}, data...)
+	return append([]byte(protocolMagic), append([]byte{byte(downLoadFileNetOrder)}, data...)...)
 }
 
 // 解码下载请求（支持带公钥的新格式和旧的纯文件名格式）
 func decodeDownloadFileInfo(data []byte) downloadRequest {
-	if len(data) < 1 {
+	magicLen := len(protocolMagic)
+	if len(data) < magicLen+1 {
 		return downloadRequest{}
 	}
 
-	if decodeOrder(data[0]) == downLoadFileNetOrder {
+	// 首先检查协议魔数
+	if string(data[:magicLen]) != protocolMagic {
+		return downloadRequest{}
+	}
+
+	if decodeOrder(data[magicLen]) == downLoadFileNetOrder {
 		req := downloadRequest{}
-		if err := json.Unmarshal(data[1:], &req); err != nil {
+		if err := json.Unmarshal(data[magicLen+1:], &req); err != nil {
 			// 尝试兼容旧的纯文件名格式
-			return downloadRequest{FileName: string(data[1:])}
+			return downloadRequest{FileName: string(data[magicLen+1:])}
 		}
 		return req
 	}
